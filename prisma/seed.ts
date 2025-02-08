0eeceedfacd776140b5f8608d6516916701a77cf
Andre Juan <139168174+AndreJetx@users.noreply.github.com>
2025-02-08 23:54:19 +0000

    preenchimento de seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -13,20 +13,55 @@ async function main() {
 
   await prisma.character.createMany({
     data: [
+
       {
-        anime: "Naruto",
+        anime: "One Piece",
+        name: "Ace",
+        power: "Mago",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-ace.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-ace.gif",
+      },
+
+      {
+        anime: "Dragon Ball Z",
         name: "Android 21",
         power: "Mago De Batalha",
         imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-androide21.png",
-        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-androide21.gif",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-android21.gif",
       },
+
+      {
+        anime: "One Piece",
+        name: "Apoo",
+        power: "Mago",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-apoo.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-apoo.gif",
+      },
+
       {
         anime: "Dragon Ball Z",
         name: "Bardok",
         power: "Lutador",
         imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-bardok.png",
-        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-bardok.gif",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-bardock.gif",
       },
+      
+      {
+        anime: "One Punch Man",
+        name: "Boros",
+        power: "Mago De Batalha",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-boros.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-boros.gif",
+      },
+
+      {
+        anime: "Bleach",
+        name: "Byakuya",
+        power: "Mago",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-byakuya.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-byakuya.gif",
+      },
+
       {
         anime: "Dragon Ball Z",
         name: "Cell",
@@ -34,6 +69,129 @@ async function main() {
         imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-cell.png",
         gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-cell.gif",
       },
+
+      {
+        anime: "Naruto",
+        name: "Choji",
+        power: "Guerreiro",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-choji.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-choji.gif",
+      },
+
+      {
+        anime: "Dragon Ball Z",
+        name: "Cooler",
+        power: "Mago De Batalha",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-cooler.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-cooler.gif",
+      },
+
+      {
+        anime: "One Piece",
+        name: "Cracker",
+        power: "Guerreiro",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-cracker.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-cracker.gif",
+      },
+
+      {
+        anime: "One Piece",
+        name: "Crocodile",
+        power: "Mago",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-crocodile.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-crocodile.gif",
+      },
+
+      {
+        anime: "Dragon Ball Z",
+        name: "Cumber",
+        power: "Guerreiro",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-cumber.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-cumber.gif",
+      },
+
+      {
+        anime: "Naruto",
+        name: "Deidara",
+        power: "Mago",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-deidara.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-deidara.gif",
+      },
+
+      {
+        anime: "One Piece",
+        name: "Doflamingo",
+        power: "Guerreiro",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-doflamingo.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-doflamingo.gif",
+      },
+
+      {
+        anime: "One Piece",
+        name: "Enel",
+        power: "Mago De Batalha",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-enel.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-enel.gif",
+      },
+
+      {
+        anime: "Dragon Ball Z",
+        name: "Freeza",
+        power: "Mago De Batalha",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-freeza.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-frieza.gif",
+      },
+
+      {
+        anime: "Naruto",
+        name: "Gaara",
+        power: "Mago",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-gaara.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-gaara.gif",
+      },
+
+      {
+        anime: "Naruto",
+        name: "Gai",
+        power: "Guerreiro",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-gai.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-gai.gif",
+      },
+
+      {
+        anime: "One Punch Man",
+        name: "Garou",
+        power: "Guerreiro",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-garou.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-garou.gif",
+      },
+
+      {
+        anime: "One Punch Man",
+        name: "Genos",
+        power: "Guerreiro",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-genos.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-genos.gif",
+      },
+
+      {
+        anime: "Fairy Tail",
+        name: "Gildarts",
+        power: "Mago",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-gildarts.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-gildarts.gif",
+      },
+
+      {
+        anime: "Dragon Ball Z",
+        name: "Gogeta",
+        power: "Mago De Batalha",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-gogeta.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-gogeta.gif",
+      },
+      
+
+
     ],
   });
 
