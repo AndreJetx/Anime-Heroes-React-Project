e646ca9e947730077d9062ddc5c18d0b090decf2
Andre Juan <139168174+AndreJetx@users.noreply.github.com>
2025-02-15 18:02:48 +0000

    fix. erros de build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ReduxProvider } from "../redux/provider";
 import '../i18n';
+import "@/styles/globals.css";
+import "@/styles/AnimeFilter.css";
+import "@/styles/CharacterCard.css";
+import "@/styles/CharacterSelection.css";
+import "@/styles/header.css";
+import "@/styles/home.css";
+import "@/styles/pagesIndex.css";
+
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
