cc56624d62d14993b1fbbb361b665a105d973be8
Andre Juan <139168174+AndreJetx@users.noreply.github.com>
2025-02-18 15:23:24 +0000

    finalizaçao da seçao dos estilos de luta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,29 +4,19 @@ import { useTranslation } from "react-i18next";
 import Image from "next/image";
 
 export default function Home() {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
 
-  if (!i18n.isInitialized) {
-    return <p>Loading...</p>;
-  }
-
-  // Função para mudar o idioma
-  const changeLanguage = (lng: "en" | "pt") => {
-    i18n.changeLanguage(lng);
-  };
 
   return (
     <>
       <Header animeList={[]} selectedAnime="" onSelectAnime={() => {}} />
       <div className="home">
         <section className="home-section">
-          <h1>{t("title")}</h1>
-          <p>{t("description")}</p>
-          <button onClick={() => changeLanguage("en")}>🇺🇸 English</button>
-          <button onClick={() => changeLanguage("pt")}>🇧🇷 Português</button>
+          <h1>{t("titlehome")}</h1>
+          <p>{t("descriptionhome")}</p>
         </section>
         <section className="style-section-container">
-          <h2>There are different types of fighters to suit your style:</h2>
+          <h2>{t("styledescription")}</h2>
           <div className="style-section">
             <div>
               <Image 
@@ -35,16 +25,34 @@ export default function Home() {
                 width={500}
                 height={300}
               />
-              <p>ARTISTA MARCIAL</p>
+              <p>{t("stylemartialartist")}</p>
             </div>
             <div>
-              
+              <Image 
+                src="/images/swordsman.png"
+                alt="Swordsman image"
+                width={500}
+                height={300}
+              />
+              <p>{t("styleswordsman")}</p>
             </div>
             <div>
-              
+              <Image 
+                src="/images/mage.png"
+                alt="Mage image"
+                width={500}
+                height={300}
+              />
+              <p>{t("stylemage")}</p>
             </div>
             <div>
-              
+              <Image 
+                src="/images/magicfist.png"
+                alt="Magic Fist image"
+                width={500}
+                height={300}
+              />
+              <p>{t("stylemagicfist")}</p>
             </div>
           </div>
         </section>
