7c9824401854149a958887d1413cd88df1c107ad
Andre Juan <139168174+AndreJetx@users.noreply.github.com>
2025-03-11 18:54:00 +0000

    Complemento de seed, e adiçao de logos dos animes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -276,7 +276,7 @@ async function main() {
         name: "Ichigo True Shikai",
         power: "styleswordsman",
         imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-ichigotrueshikai.png",
-        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-ichigotrueshikai.gif",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-Ichigotrueshikai.gif",
       },
 
       {
@@ -542,6 +542,271 @@ async function main() {
         imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-ogichi.png",
         gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-ogichi.gif",
       },
+
+      {
+        anime: "Naruto",
+        name: "Pain",
+        power: "stylemagicfist",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-pain.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-pain.gif",
+      },
+
+      {
+        anime: "Dragon Ball Z",
+        name: "Piccolo",
+        power: "stylemagicfist",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-piccolo.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-piccolo.gif",
+      },
+
+      {
+        anime: "Dragon Ball Z",
+        name: "Piccolo Daimaoh",
+        power: "stylemagicfist",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-piccolodaimaoh.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-piccolodaimaoh.gif",
+      },
+
+      {
+        anime: "Naruto",
+        name: "Raikage",
+        power: "stylemartialartist",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-raikage.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-raikage.gif",
+      },
+
+      {
+        anime: "Naruto",
+        name: "Rock Lee",
+        power: "stylemartialartist",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-rocklee.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-rocklee.gif",
+      },
+
+      {
+        anime: "Fairy Tail",
+        name: "Rogue",
+        power: "stylemage",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-rogue.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-rogue.gif",
+      },
+
+      {
+        anime: "Naruto",
+        name: "Sai",
+        power: "stylemage",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-sai.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-sai.gif",
+      },
+
+      {
+        anime: "Naruto",
+        name: "Sakura",
+        power: "stylemartialartist",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-sakura.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-sakura.gif",
+      },
+
+      {
+        anime: "One Piece",
+        name: "Sanji",
+        power: "stylemartialartist",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-sanji.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-sanji.gif",
+      },
+
+      {
+        anime: "Naruto",
+        name: "Sasuke",
+        power: "stylemagicfist",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-sasuke.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-sasuke.gif",
+      },
+
+      {
+        anime: "Outros",
+        name: "Shadow",
+        power: "stylemartialartist",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-shadow.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-shadow.gif",
+      },
+
+      {
+        anime: "Naruto",
+        name: "Shino",
+        power: "stylemagicfist",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-shino.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-shino.gif",
+      },
+
+      {
+        anime: "Pokemon",
+        name: "Snorlax",
+        power: "stylemartialartist",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-snorlax.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-snorlax.gif",
+      },
+
+      {
+        anime: "Outros",
+        name: "Sonic",
+        power: "stylemartialartist",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-sonic.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-sonic.gif",
+      },
+
+      {
+        anime: "Fairy Tail",
+        name: "Sting",
+        power: "stylemage",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-sting.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-sting.gif",
+      },
+
+      {
+        anime: "Naruto",
+        name: "Suigetsu",
+        power: "styleswordsman",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-suigetsu.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-suigetsu.gif",
+      },
+
+      {
+        anime: "Dragon Ball Z",
+        name: "Tapion",
+        power: "styleswordsman",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-tapion.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-tapion.gif",
+      },
+
+      {
+        anime: "Dragon Ball Z",
+        name: "Tenshinhan",
+        power: "stylemagicfist",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-tenshinhan.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-tenshinhan.gif",
+      },
+
+      {
+        anime: "Dragon Ball Z",
+        name: "Toppo",
+        power: "stylemagicfist",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-toppo.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-toppo.gif",
+      },
+
+      {
+        anime: "Dragon Ball Z",
+        name: "Trunks Super",
+        power: "styleswordsman",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-trunks.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-trunkssuper.gif",
+      },
+
+      {
+        anime: "Dragon Ball Z",
+        name: "Trunks DBZ",
+        power: "styleswordsman",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-trunksDBZ.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-trunksDBZ.gif",
+      },
+
+      {
+        anime: "Bleach",
+        name: "Ulquiorra",
+        power: "stylemagicfist",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-ulquiorra.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-ulquiorra.gif",
+      },
+
+      {
+        anime: "Dragon Ball Z",
+        name: "Vegeta",
+        power: "stylemagicfist",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-vegeta.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-vegeta.gif",
+      },
+
+      {
+        anime: "Dragon Ball Z",
+        name: "Vegeta Scouter",
+        power: "stylemagicfist",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-vegetascouter.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-vegetascouter.gif",
+      },
+
+      {
+        anime: "Dragon Ball Z",
+        name: "Vegetto",
+        power: "stylemagicfist",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-vegetto.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-vegetto.gif",
+      },
+
+      {
+        anime: "Naruto",
+        name: "Yagura",
+        power: "stylemagicfist",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-yagura.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-yagura.gif",
+      },
+
+      {
+        anime: "Bleach",
+        name: "Yamamoto",
+        power: "styleswordsman",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-yamamoto.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-yamamoto.gif",
+      },
+
+      {
+        anime: "Dragon Ball Z",
+        name: "Yamcha",
+        power: "stylemagicfist",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-yancha.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-yamcha.gif",
+      },
+
+      {
+        anime: "Dragon Ball Z",
+        name: "Zamassu",
+        power: "stylemagicfist",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-zamassu.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-zamassu.gif",
+      },
+
+      {
+        anime: "Dragon Ball Z",
+        name: "Potara Zamassu",
+        power: "stylemagicfist",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-zamassufused.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-fusedzamassu.gif",
+      },
+
+      {
+        anime: "Bleach",
+        name: "Zaraki",
+        power: "styleswordsman",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-zaraki.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-zaraki.gif",
+      },
+
+      {
+        anime: "Bleach",
+        name: "Zaraki TYBW",
+        power: "styleswordsman",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-zarakitybw.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-zarakiTYBW.gif",
+      },
+
+      {
+        anime: "One Piece",
+        name: "Zoro",
+        power: "styleswordsman",
+        imageUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-zoro.png",
+        gifUrl: "https://animeheroes.s3.sa-east-1.amazonaws.com/card-zoro.gif",
+      },
+
 
 
     ],
